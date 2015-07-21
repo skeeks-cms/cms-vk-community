3718ff7fe1df026aee2f53e09977028fa30e1bcb
Неля <nelya@rusoft.ru>
2015-07-21 13:58:16 +0300

    90378 Разработка виджета сообщест вконтакте.

--- views/_settingsForm.php
+++ views/_settingsForm.php
@@ -14,7 +14,9 @@ use skeeks\cms\modules\admin\widgets\form\ActiveFormUseTab as ActiveForm;
     <?= $form->fieldSetEnd(); ?>
 
     <?= $form->fieldSet('Параметры виджета'); ?>
-        <?= $form->fieldInputInt($model, 'apiId')->hint('http://vk.com/pages?oid=-4489985&p=%D0%9A%D0%B0%D0%BA_%D1%83%D0%B7%D0%BD%D0%B0%D1%82%D1%8C_id_%D0%B3%D1%80%D1%83%D0%BF%D0%BF%D1%8B_%D0%B8%D0%BB%D0%B8_%D0%BF%D1%80%D0%BE%D1%84%D0%B8%D0%BB%D1%8F%3F - посмотреть тут'); ?>
+        <?= $form->fieldInputInt($model, 'apiId'); ?>
+        <p>Как получить id приложения подробно описано <a href="http://vk.com/pages?oid=-4489985&p=%D0%9A%D0%B0%D0%BA_%D1%83%D0%B7%D0%BD%D0%B0%D1%82%D1%8C_id_%D0%B3%D1%80%D1%83%D0%BF%D0%BF%D1%8B_%D0%B8%D0%BB%D0%B8_%D0%BF%D1%80%D0%BE%D1%84%D0%B8%D0%BB%D1%8F%3F" target="_blank">здесь</a>.</p>
+
         <?= $form->field($model, 'selectMode')->radioList(\skeeks\cms\vk\community\VkCommunityWidget::$possibleMode); ?>
 
         <?= $form->fieldCheckboxBoolean($model, 'wide')->hint('используется только для новостей'); ?>
